6855c84ddcbcd091c4b16a802c31bffaec1f27d9
aliansyahFirdaus <aliansyah.it@gmail.com>
2023-08-28 12:45:24 +0700

    init

---
+++ components/envs/my-react-native-env/config/prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * @see https://bit.dev/reference/prettier/prettier-config
+ */
+const { prettierConfig } = require("@teambit/react.react-native-env");
+
+module.exports = {
+    ...prettierConfig,
+};
+
+
+
+
+
---
+++ components/envs/my-react-native-env/index.ts
@@ -0,0 +1,8 @@
+import { MyReactNativeEnv } from "./my-react-native-env.bit-env";
+export { MyReactNativeEnv };
+export default MyReactNativeEnv;
+
+
+
+
+
